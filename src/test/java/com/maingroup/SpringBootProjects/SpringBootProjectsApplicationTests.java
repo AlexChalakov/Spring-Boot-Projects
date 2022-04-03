7d2654a40a1d17350c8885d2037d000b3d1dc7f4
Alexcheto <a.chalakov@lancaster.ac.uk>
2022-04-03 23:48:03 +0100

    init commit

---
+++ src/test/java/com/maingroup/SpringBootProjects/SpringBootProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maingroup.SpringBootProjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
